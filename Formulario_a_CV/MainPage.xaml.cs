adaca59fa8cf179a6175678fe4156328e9b735b1
JeanMSalazar <113962946+JeanMSalazar@users.noreply.github.com>
2022-12-14 23:37:43 -0500

    Add files via upload

---
+++ Formulario_a_CV/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Formulario_a_CV
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void ButtonSubmitClicked(System.Object sender, System.EventArgs e)
+        {
+            var input_name = name.Text;
+            var input_lastname = lastname.Text;
+            var input_age = age.Text;
+            var input_occupation = occupationplus.Text;
+            var input_language = languageplus.Text; 
+            var input_address = address.Text;
+            var input_abilities = abilitieplus.Text;
+            var input_formation = formationplus.Text;
+            var input_w_experience = w_experienceplus.Text; 
+
+            await Application.Current.MainPage.Navigation.PushModalAsync(
+                new Page1
+                (
+                input_name,
+                input_lastname,
+                input_age, 
+                input_occupation,
+                input_language,
+                input_address,
+                input_abilities,
+                input_formation,
+                input_w_experience
+                ), true);
+
+        }
+
+        void ButtonOcupationPlusClicked(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your occupation" };
+            ocupation.Children.Add(entry);
+        }
+
+        void ButtonLanguagesPlusClicked(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your languages" };
+            language.Children.Add(entry);
+        }
+
+        void ButtonAbilitiesPlusClicked(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your abilities" };
+            abilitie.Children.Add(entry);
+        }
+
+        void ButtonFormationPlusClicked(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your formation" };
+            formation.Children.Add(entry);
+        }
+        void ButtonExperiencePlusClicked(System.Object sender, System.EventArgs e)
+        {
+            var entry = new Entry() { Placeholder = "Enter your work experience" };
+            w_experience.Children.Add(entry);
+        }
+
+    }
+}
---
+++ Formulario_a_CV/Page1.xaml.cs
@@ -0,0 +1,158 @@
+﻿using PdfSharp.Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.Xaml;
+
+
+namespace Formulario_a_CV
+{
+	public partial class Page1 : ContentPage
+	{
+		private string mainContentName;
+		private string mainContentLastname;
+		private string mainContentAge;
+		private string mainContentOccupation;
+		private string mainContentLanguage;
+		private string mainContentAddress;
+		private string mainContentAbilities;
+		private string mainContentFormation;
+		private string mainContentW_experience;
+
+		public string MainContentW_experience
+		{
+			get { return mainContentW_experience; }
+			set
+			{
+				mainContentW_experience = value;
+				OnPropertyChanged(nameof(MainContentW_experience));
+			}
+		}
+		public string MainContentFormation
+		{
+			get { return mainContentFormation; }
+			set
+			{
+				mainContentFormation = value;
+				OnPropertyChanged(nameof(MainContentFormation));
+			}
+		}
+		public string MainContentAbilities
+		{
+			get { return mainContentAbilities; }
+			set
+			{
+				mainContentAbilities = value;
+				OnPropertyChanged(nameof(MainContentAbilities));
+			}
+		}
+		public string MainContentAddress
+		{
+			get { return mainContentAddress; }
+			set
+			{
+				mainContentAddress = value;
+				OnPropertyChanged(nameof(MainContentAddress));
+			}
+		}
+		public string MainContentLanguage
+		{
+			get { return mainContentLanguage; }
+			set
+			{
+				mainContentLanguage = value;
+				OnPropertyChanged(nameof(MainContentLanguage));
+			}
+		}
+		public string MainContentOccupation
+		{
+			get { return mainContentOccupation; }
+			set
+			{
+				mainContentOccupation = value;
+				OnPropertyChanged(nameof(MainContentOccupation));
+			}
+		}
+		public string MainContentAge
+		{
+			get { return mainContentAge; }
+			set
+			{
+				mainContentAge = value;
+				OnPropertyChanged(nameof(MainContentAge));
+			}
+		}
+		public string MainContentLastname
+		{
+			get { return mainContentLastname; }
+			set
+			{
+				mainContentLastname = value;
+				OnPropertyChanged(nameof(MainContentLastname));
+			}
+		}
+		public string MainContentName
+		{
+			get { return mainContentName; }
+			set
+			{
+				mainContentName = value;
+				OnPropertyChanged(nameof(MainContentName));
+			}
+		}
+
+		public Page1(
+			   string mainContentName,
+			   string mainContentLastname,
+			   string mainContentAge, 
+			   string mainContentOccupation, 
+			   string mainContentLanguage, 
+			   string mainContentAddress,
+			   string mainContentAbilities,
+			   string mainContentFormation,
+			   string mainContentW_experience)
+        {
+            InitializeComponent();
+            BindingContext = this;
+            MainContentName = mainContentName;
+			MainContentLastname = mainContentLastname;
+            MainContentAge = mainContentAge;
+            MainContentOccupation = mainContentOccupation;
+			MainContentLanguage = mainContentLanguage;
+			MainContentAddress = mainContentAddress;
+			MainContentAbilities = mainContentAbilities;
+			MainContentFormation = mainContentFormation;
+			MainContentW_experience = mainContentW_experience;
+        }
+
+		private async void Button_Save_CV(object sender, EventArgs e)
+		{
+			var pdf = PDFManager.GeneratePDFFromView(PagetoPDF);
+			string fn = "CV.pdf";
+			var basspath = System.IO.Path.Combine(FileSystem.CacheDirectory, fn);
+			pdf.Save(basspath);
+
+			var message = new EmailMessage
+			{
+				Subject = "CV from application",
+				Body = "Hello, this is your CV from application",
+			};
+
+			message.Attachments.Add(new EmailAttachment(basspath));
+			await Email.ComposeAsync(message);
+
+		}
+
+	}
+
+}
+
+
